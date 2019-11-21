3f431afd3230341d5d81802c109ca070e9cf4757
Nathan Sanford <47925424+natsan91@users.noreply.github.com>
2019-11-21 12:15:08 -0600

    Add IS tutorial materials

--- Multiple_ISGaussian.m
+++ Multiple_ISGaussian.m
@@ -1,45 +1,38 @@
-function ISGaussian(Perform_IS)
-% importance sampling for the Gaussian random walk and compares with exact
-% answer by plotting exact pdf with numerically created approximations (by
-% scaling histograms appropriately)
-% Allows for turning IS on or off. Without IS, code still does MC.
-% If Perform_IS==1, IS is performed. Not performed otherwise or if not
-% specified.
+function Multiple_ISGaussian
+% multiple importance sampling for the Gaussian random walk
+% produces a full histogram which closely matches the exact solution
+% by using multiple importance sampling targets.
 % Written by Nathan Sanford to accompany Importance_Sampling_Tutorial.pdf
 
-rng(5) % ensures replicability of results (comment out or change number to get different)
-% importance sampling on or off
-if nargin==0 || Perform_IS ~= 1
-    fprintf('Not performing importance sampling \n')
-    Perform_IS=0;
-else
-    fprintf('Performing importance sampling \n')
-end
+% importance sampling on 
+Perform_IS = 1;
+% number of distributions to use
+J = 6;
 
-Ovar=1; % variance of gaussian distribution
+% details about gaussian process and numerical parameters
+Ovar=1; % variance of original distribution
 N=10; % number of gaussians in sum
 gaussian=@(x,mu,v) 1/sqrt(2*pi*v).*exp(-(x-mu).^2/(2*v)); 
 numSamples=10^5; 
 numBins=101;
-% locs are bin centers
+% locs are bin centers for histogram
 locs=linspace(-25,25,numBins);
 binwidth = locs(2)-locs(1);
 
 if Perform_IS
-    IS_target=10; % biasing target(s)
-    J=length(IS_target); % code technically set up to allow multiple IS
-    mu=IS_target/N; % mean shift for biasing distribution
+    IS_target=linspace(-15,15,J); % biasing target(s)
+    mu=IS_target/N; % mean shifts for each target
     % apportion total samples up amongst the targets
     M=floor(numSamples/J)*ones(J,1); 
     M(ceil(J/2))=M(ceil(J/2))+numSamples-sum(M);
     % pre-allocate arrays for prob and variance histograms (for each)
     PhatJ=zeros(J,length(locs)); 
-    ShatJ=zeros(J,length(locs));
+    ShatJ=zeros(J,length(locs)); 
 end
 
 %% direct monte-carlo sims
 binmc = zeros(1,numSamples);
-% Straight monte-carlo
+% Straight monte-carlo 
 for i=1:numSamples
     xmc = sqrt(Ovar)*randn(N,1);
     zz = sum(xmc);
@@ -47,15 +40,15 @@ for i=1:numSamples
 end
 % compute variance along whole distribution (there are faster ways to do
 % this but this code is good for demonstration purposes)
-mcP = zeros(1,numBins); mcVar = mcP; 
+mcP = zeros(1,numBins); mcV = mcP; 
 for j = 1:numBins
     inds = find(binmc == j);
     mcP(j) = length(inds)/numSamples/binwidth;
-    mcVar(j) = sum(((binmc==j)-mcP(j)).^2);
+    mcV(j) = sum(((binmc==j)-mcP(j)).^2);
 end
 
-mcVar = 1/(numSamples*(numSamples-1))*mcVar; % monte carlo variance estimator
-mc_cov = sqrt(mcVar)./mcP; % monte carlo coeff of var estimator
+mcV = 1/(numSamples*(numSamples-1))*mcV;
+mcCV = sqrt(mcV)./mcP;
 
 %% Using Importance sampling
 if Perform_IS
@@ -76,7 +69,7 @@ if Perform_IS
             for jj=1:J
                biased(jj)=prod(gaussian(xstar,mu(jj),Ovar),1);
             end
-            invlike=biased./prod(gaussian(xstar,0,Ovar)); % reciprocal of likelihood ratio
+            invlike=biased./prod(gaussian(xstar,0,Ovar)); 
             binweight(k)=1/sum(M'.*invlike);
         end
 
@@ -93,15 +86,15 @@ if Perform_IS
             ShatJ(j,:)=ShatJ(j,:)+temp.^2;
         end
     end
-    
-    P_IS=sum(PhatJ,1);
+
+    Phat_IS=sum(PhatJ,1); 
     var_IS=sum(1./(M.*(M-1)).*ShatJ,1); 
-    cov_IS=sqrt(var_IS)./P_IS; 
+    cov_IS=sqrt(var_IS)./Phat_IS; 
 end
 
 %% Plotting
 if Perform_IS
-    xbds=[0 20];
+    xbds=[-20 20];
     ybds=[1e-10 1];
 else
     xbds=[-15 15];
@@ -113,15 +106,16 @@ semilogy(locs,gaussian(locs,0,N*Ovar),'-g',locs,mcP,'--b','linewidth',2)
 legendinfo={'Exact','MC'};
 hold on
 if Perform_IS
-    semilogy(locs,P_IS,':r','linewidth',2)
+    semilogy(locs,Phat_IS,':r','linewidth',2)
     for h=1:J
-        plot(IS_target(h)*ones(100,1),linspace(1e-100,1),'--',...
-            'color',0.7*ones(1,3),'linewidth',2)
+        semilogy(locs,PhatJ(h,:),'k')
+        if h==1 || h==J
+            plot(IS_target(h)*ones(100,1),linspace(1e-100,1),'--',...
+                'color',0.7*ones(1,3),'linewidth',2)
+        end
     end
-    legendinfo{3}='ISMC estimate';
-    legend(legendinfo,'location','southwest','fontsize',10.5)
-else
-    legend(legendinfo,'location','south','fontsize',10.5)
+    legendinfo{3}='MISMC estimate';
+    legendinfo{4}='Individual contributions';
 end
 yticks(10.^(-100:2:0))
 title(sprintf('10^{%d} samples of Gaussian random walk',...
@@ -131,16 +125,21 @@ axis([xbds,ybds])
 hold off
 
 subplot(2,1,2)
-semilogy(locs,mc_cov,'--b','linewidth',2)
+semilogy(1e5,1e5,'g','linewidth',2)  % for legend
 hold on
+semilogy(locs,mcCV,'--b','linewidth',2)
 if Perform_IS
     semilogy(locs,cov_IS,':r','linewidth',2)
-    for h=1:J
+    plot(1e5,1e5,'k') % for legend
+    for h=[1,J]
         plot(IS_target(h)*ones(100,1),linspace(1e-10,1),'--',...
             'color',0.7*ones(1,3),'linewidth',2)
     end
 end
-axis([xbds, min(10^-2.5,min(mc_cov(mc_cov>0))) 1])
+axis([xbds, min(10^-2.5,min(mcCV(mcCV>0))) 1])
+% put legend in between subplots for maximal readability
+myleg=legend(legendinfo,'location','north','fontsize',10.5);
+myleg.Position(2) = myleg.Position(2)+0.1;  
 ylabel('C.V.')
 xlabel(['Z_{',num2str(N),'}'])
 hold off
